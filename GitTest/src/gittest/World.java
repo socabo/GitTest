73d228232fcc44d7c1d153c1d4caeffeeca9ef99
Ilidio Lopes <depil01@ca.com>
2017-05-26 11:53:53 +0200

    Creating World class

---
+++ GitTest/src/gittest/World.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class World {
+
+	public World() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
